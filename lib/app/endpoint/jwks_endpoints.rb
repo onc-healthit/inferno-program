77524be423ef367f1112eb3a90e52f29947ab93b
Yunwei Wang <yunwwang@gmail.com>
2020-02-12 08:19:08 -0500

    change .present? to .method_defined?

--- lib/app/endpoint/jwks_endpoints.rb
+++ lib/app/endpoint/jwks_endpoints.rb
@@ -6,7 +6,20 @@ module Inferno
       def self.included(klass)
         klass.class_eval do
           get '/.well-known/jwks.json' do
-            an_instance = Inferno::Models::TestingInstance.first()
+            binding.pry
+            if settings.method_defined? :bulk_data_jwks
+              keys = []
+              if settings.bulk_data_jwks['es384_public'].present?
+                keys.push(settings.bulk_data_jwks['es384_public'])
+              end
+              if settings.bulk_data_jwks['es384_public'].present?
+                keys.push(settings.bulk_data_jwks['es384_public'])
+              end
+            end
+
+            jwks_urls = Hash.new()
+            jwks_urls["keys"] = keys
+
             content_type :json
             binding.pry
             an_instance.bulk_public_key
