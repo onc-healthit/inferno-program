e831fc704911e92b3e133c340453f135c1b21e65
Adamson, Reece L <radamson@mitre.org>
2020-02-28 10:06:01 -0500

    Apply suggestion to lib/app/utils/test/bcp47_test.rb

--- lib/app/utils/test/bcp47_test.rb
+++ lib/app/utils/test/bcp47_test.rb
@@ -7,7 +7,7 @@ describe Inferno::BCP47 do
   before do
     @bcp47 = Inferno::BCP47
   end
-  it 'loads can load all languages' do
+  it 'can load all languages' do
     @bcp47.filter_codes.length == 8152
   end
 end
