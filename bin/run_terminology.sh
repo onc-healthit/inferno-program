d1635f81aac1fbf84bcab66bfc0a3185e1bcd430
Michael O'Keefe <mokeefe@mitre.org>
2020-03-05 08:09:22 -0500

    Better trap signals in Dockerland

--- bin/run_terminology.sh
+++ bin/run_terminology.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 temp_folder="tmp/terminology"
 
@@ -6,56 +7,63 @@ if [[ ! -f "$temp_folder/umls.zip" ]]; then
   [[ -z "$UMLS_USERNAME" ]] && { echo "UMLS Username environment variable is empty" >&2 ; exit 1; }
   [[ -z "$UMLS_PASSWORD" ]] && { echo "UMLS Password environment variable is empty" >&2 ; exit 1; }
   function control_c_download {
+    kill -INT "$child"
     rm -f "$temp_folder/umls.zip"
     exit $?
   }
-  trap control_c_download SIGINT
-  trap control_c_download SIGTERM
-  bundle exec rake terminology:download_umls["$UMLS_USERNAME","$UMLS_PASSWORD"]
+  trap control_c_download SIGHUP SIGINT SIGTERM SIGQUIT SIGKILL SIGTSTP
+  bundle exec rake terminology:download_umls["$UMLS_USERNAME","$UMLS_PASSWORD"] &
+  child=$!
+  wait "$child"
 else
   echo "UMLS Zip already exists; skipping UMLS download Rake task."
 fi
 
 if [[ ! -e "$temp_folder/umls" ]]; then
   function control_c_unzip {
+    kill -INT "$child"
     rm -rf "$temp_folder/umls"
     exit $?
   }
-  trap control_c_unzip SIGINT
-  trap control_c_unzip SIGTERM
-  bundle exec rake terminology:unzip_umls
+  trap control_c_unzip SIGHUP SIGINT SIGTERM SIGQUIT SIGKILL SIGTSTP
+  bundle exec rake terminology:unzip_umls &
+  child=$!
+  wait "$child"
 else
   echo "UMLS Directory already exists; skipping UMLS Unzip Rake task."
 fi
 
 if [[ ! -e "$temp_folder/umls_subset" ]]; then
   function control_c_mmsys {
+    kill -INT "$child"
     rm -rf "$temp_folder/umls_subset"
     exit $?
   }
-  trap control_c_mmsys SIGINT
-  trap control_c_mmsys SIGTERM
-  bundle exec rake terminology:run_umls
+  trap control_c_mmsys SIGHUP SIGINT SIGTERM SIGQUIT SIGKILL SIGTSTP
+  bundle exec rake terminology:run_umls &
+  child=$!
+  wait "$child"
 else
   echo "UMLS Subset already exists; skipping Metamorphosys Rake task."
 fi
 
 if [[ ! -f "$temp_folder/umls.db" ]]; then
   function control_c_db {
+    kill -INT "$child"
     rm -f "$temp_folder/umls.db"
     rm -f "$temp_folder/MRCONSO.pipe"
     rm -f "$temp_folder/MRREL.pipe"
     rm -f "$temp_folder/MRSAT.pipe"
     exit $?
   }
-  trap control_c_db SIGINT
-  trap control_c_db SIGTERM
-  ./bin/create_umls.sh
+  trap control_c_db SIGHUP SIGINT SIGTERM SIGQUIT SIGKILL SIGTSTP
+  ./bin/create_umls.sh &
+  child=$!
+  wait "$child"
 else
   echo "UMLS DB Already exists; skipping creation script."
 fi
 
-trap - SIGINT
-trap - SIGTERM
+trap - SIGHUP SIGINT SIGTERM SIGQUIT SIGKILL SIGTSTP
 
-bundle exec rake terminology:create_vs_validators
+exec bundle exec rake terminology:create_vs_validators
