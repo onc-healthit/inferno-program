9919feb6e04aef222ecc70c91281481ad19b1c0c
Michael O'Keefe <mokeefe@mitre.org>
2020-03-06 11:46:33 -0500

    Add package download to run_terminology

--- bin/run_terminology.sh
+++ bin/run_terminology.sh
@@ -47,6 +47,20 @@ else
   echo "UMLS Subset already exists; skipping Metamorphosys Rake task."
 fi
 
+if [[ ! -f "$temp_folder/fhir" ]]; then
+  function control_c_packages {
+    kill -INT "$child"
+    rm -rf "$temp_folder/fhir"
+    exit $?
+  }
+  trap control_c_packages SIGHUP SIGINT SIGTERM SIGQUIT SIGKILL SIGTSTP
+  bundle exec rake terminology:download_program_terminology &
+  child=$!
+  wait "$child"
+else
+  echo "FHIR Package sources already exist; skipping Package download."
+fi
+
 if [[ ! -f "$temp_folder/umls.db" ]]; then
   function control_c_db {
     kill -INT "$child"
