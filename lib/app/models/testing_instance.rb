38026669f9dd20bfabfefb592354965efb4016e2
Reece Adamson <radamson@mitre.org>
2020-03-09 16:05:01 -0400

    reorder group_result to fix results rollup

--- lib/app/models/testing_instance.rb
+++ lib/app/models/testing_instance.rb
@@ -318,10 +318,10 @@ module Inferno
       private
 
       def group_result(results)
-        return :skip if results[:skip].positive?
         return :fail if results[:fail].positive?
         return :fail if results[:cancel].positive?
         return :error if results[:error].positive?
+        return :skip if results[:skip].positive?
         return :not_run if results[:total].zero?
 
         :pass
