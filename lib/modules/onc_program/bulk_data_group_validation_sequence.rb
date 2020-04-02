db2bcc90f8e1af48a35dd59bf95ec79e1ac28327
Yunwei Wang <yunwwang@gmail.com>
2020-04-02 11:09:33 -0500

    Remove warnings when using FHIRModelsValidator

--- lib/modules/onc_program/bulk_data_group_validation_sequence.rb
+++ lib/modules/onc_program/bulk_data_group_validation_sequence.rb
@@ -87,7 +87,10 @@ module Inferno
 
           p = Inferno::ValidationUtil.guess_profile(resource, @instance.fhir_version.to_sym)
           if p && @instance.fhir_version == 'r4'
-            errors = Inferno::RESOURCE_VALIDATOR.validate(resource, versioned_resource_class(klass), p.url)
+            errors = Inferno::RESOURCE_VALIDATOR.validate(resource, versioned_resource_class, p.url)
+
+            # Remove warnings if using internal FHIRModelsValidator. FHIRModelsValidator has an issue with FluentPath.
+            errors = [] if errors[:errors].empty? && Inferno::RESOURCE_VALIDATOR.is_a?(Inferno::FHIRModelsValidator)
           else
             warn { assert false, 'No profiles found for this Resource' }
             errors = resource.validate
