c5c314ab933db8e9dec9be6a40afb70e4427baf0
Rob Scanlon <robscanlon@gmail.com>
2020-06-01 23:57:45 -0400

    Updates for v1.0.0

--- lib/modules/onc_program/onc_ehr_launch_sequence.rb
+++ lib/modules/onc_program/onc_ehr_launch_sequence.rb
@@ -51,7 +51,8 @@ module Inferno
           'Practitioner',
           'PractitionerRole',
           'Procedure',
-          'Provenance'
+          'Provenance',
+          'RelatedPerson'
         ]
       end
 
