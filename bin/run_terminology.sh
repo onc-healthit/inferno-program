e9f5dcb3db624aff1a7d42767ee6f578a90e35ec
Stephen MacVicar <smacvicar@mitre.org>
2020-06-05 12:39:08 -0400

    only perform memory check when running in docker

--- bin/run_terminology.sh
+++ bin/run_terminology.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-available_ram_kb=$(cat /proc/meminfo | grep MemTotal | sed -r 's/MemTotal:\s+([0-9]+)\skB/\1/')
-(( available_ram_gb=$available_ram_kb / 1000 / 1000 ))
-if (( $available_ram_gb < 10 )); then
+# Check available memory when running in docker
+if [ -f /.dockerenv ]; then
+  available_ram_kb=$(cat /proc/meminfo | grep MemTotal | sed -r 's/MemTotal:\s+([0-9]+)\skB/\1/')
+  (( available_ram_gb=$available_ram_kb / 1000 / 1000 ))
+  if (( $available_ram_gb < 10 )); then
     echo "10 GB of RAM must be available in order to process the terminology, but only $available_ram_gb GB are available"
     exit 1
+  fi
 fi
 
 temp_folder="tmp/terminology"
