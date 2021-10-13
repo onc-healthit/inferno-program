f6473e9af5d48715c2187668f7345b89ebc76237
Yunwei Wang <yunwwang@gmail.com>
2021-10-13 15:57:51 -0500

    Add clarification to test description

--- lib/modules/uscore_v3.1.1/us_core_documentreference_sequence.rb
+++ lib/modules/uscore_v3.1.1/us_core_documentreference_sequence.rb
@@ -729,6 +729,7 @@ module Inferno
             * content
             * content.attachment
             * content.attachment.contentType
+            * content.attachment.data or content.attachment.url
             * content.format
             * context
             * context.encounter
