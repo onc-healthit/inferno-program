a4fdd4197d0873ace29a10758cda199a26cb7790
Stephen MacVicar <Jammjammjamm@users.noreply.github.com>
2021-11-23 13:06:48 -0500

    FI-1364: Fix duplicated search (#395)
    * remove duplicated code in generator
    
    * regenerate us core

--- generator/uscore/uscore_generator.rb
+++ generator/uscore/uscore_generator.rb
@@ -1234,9 +1234,6 @@ module Inferno
               assert_bundle_response(reply)
 
               #{search_by_type_length_check unless sequence[:resource] == 'Device'}
-              search_with_type = fetch_all_bundled_resources(reply, check_for_data_absent_reasons)
-              search_with_type.select! { |resource| resource.resourceType == '#{sequence[:resource]}'}
-              assert search_with_type.length == resources_returned.length, 'Expected search by Patient/ID to have the same results as search by ID'
 
               # Search by POST variant
               reply = get_resource_by_params(versioned_resource_class('#{sequence[:resource]}'), search_params, search_method: :post)
