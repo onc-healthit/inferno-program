fbc1af48facb6a3b27679d17b501d9713b7a713e
Yunwei Wang <yunwwang@gmail.com>
2021-12-16 12:17:51 -0600

    Allow case-insensitive comparasion for token search

--- lib/modules/uscore_v3.1.1/us_core_diagnosticreport_lab_sequence.rb
+++ lib/modules/uscore_v3.1.1/us_core_diagnosticreport_lab_sequence.rb
@@ -92,9 +92,9 @@ module Inferno
           coding_value = value.split('|').last
           match_found = values_found.any? do |codeable_concept|
             if value.include? '|'
-              codeable_concept.coding.any? { |coding| coding.system == coding_system && coding.code == coding_value }
+              codeable_concept.coding.any? { |coding| coding.system == coding_system && coding.code.casecmp?(coding_value) }
             else
-              codeable_concept.coding.any? { |coding| coding.code == value }
+              codeable_concept.coding.any? { |coding| coding.code.casecmp?(value) }
             end
           end
           assert match_found, "category in DiagnosticReport/#{resource.id} (#{values_found}) does not match category requested (#{value})"
@@ -105,9 +105,9 @@ module Inferno
           coding_value = value.split('|').last
           match_found = values_found.any? do |codeable_concept|
             if value.include? '|'
-              codeable_concept.coding.any? { |coding| coding.system == coding_system && coding.code == coding_value }
+              codeable_concept.coding.any? { |coding| coding.system == coding_system && coding.code.casecmp?(coding_value) }
             else
-              codeable_concept.coding.any? { |coding| coding.code == value }
+              codeable_concept.coding.any? { |coding| coding.code.casecmp?(value) }
             end
           end
           assert match_found, "code in DiagnosticReport/#{resource.id} (#{values_found}) does not match code requested (#{value})"
